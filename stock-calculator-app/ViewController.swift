dca2923d78dde1c5a843b174a58b44aa92f5bda4
Filip Hercig <fi.hercig@gmail.com>
2022-04-27 15:04:28 +0200

    Initial Commit

---
+++ stock-calculator-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stock-calculator-app
+//
+//  Created by Filip Hercig on 27.04.2022..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
